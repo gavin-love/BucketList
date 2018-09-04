57103309cf9a9e1e210a53fd121dbc2bf22b8780
gavin-love <1co.loco@oprotonmail.ch>
2018-09-04 10:09:54 -0600

    Add DELETE from db by name to server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.post('/api/v1/bucket_lists', (req, resp) => {
 
   for (let requiredParameter of ['title', 'description']) {
     if (!bucketList[requiredParameter]) {
-      return Response.status(422).json({
+      return resp.status(422).json({
         error: `Your bucket list is missing a required parameter of "${requiredParameter}"`
       });
     };
@@ -23,20 +23,36 @@ app.post('/api/v1/bucket_lists', (req, resp) => {
 
   database('bucket_list').insert(bucketList, 'id')
     .then(bucket_list => {
-      return resp.status(201).json({ id: bucket_list[0] })
+      return resp.status(201).json({ id: bucket_list[0] });
     })
     .catch(error => {
-      return resp.status(500).json({ error })
+      return resp.status(500).json({ error });
     });
 });
 
 app.get('/api/v1/bucket_lists', (req, resp) => {
   database('bucket_list').select()
     .then(bucketLists => {
-      return res.status(200).json(bucketLists);
+      return resp.status(200).json(bucketLists);
     })
     .catch(error => {
-      return res.status(500).json({ error })
+      return resp.status(500).json({ error });
+    });
+});
+
+app.delete('/api/v1/bucket_lists/:title', (req, resp) => {
+  const title = request.params.title;
+
+  database('bucket_list').where('title', title).select().del()
+    .then(bucketList => {
+      if (bucketList) {
+        return resp.status(200).json('Bucket list was deleted');
+      } else {
+        return resp.status(404).json('The bucket list you are looking for does not exist in the database');
+      };
     })
-})
+    .catch(error => {
+      return resp.status(500).json({ error });
+    });
+});
 
