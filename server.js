d243f4df1d55c53b56f8f92fd82b91b2079c1840
gavin-love <1co.loco@oprotonmail.ch>
2018-09-04 09:43:33 -0600

    Configure knex to use development environment and express to serve up public directory

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const environment = process.end.NODE_env || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser).urlencoded({ extended: true });
+
+
+
